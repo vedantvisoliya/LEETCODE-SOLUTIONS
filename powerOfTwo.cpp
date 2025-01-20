47c0bf3605907802ed52fcecddefab7c0ccce99e
Vedant Kumar <vedantvisoliya@gmail.com>
2025-01-20 12:22:28 +0530

    made some changes

--- powerOfTwo.cpp
+++ powerOfTwo.cpp
@@ -3,27 +3,18 @@
 using namespace std;
 
 bool isPowerOfTwo(int n) {
-    if(n < 0){
+    if (n > 0){
         for(int i = 0; i <= 31; i++){
-            if (pow(-2,i) == n){
-                return true;
+                if(pow(2,i) == n){
+                    return true;
+                }
             }
-        }
-        return false;
     }
-    else{
-        for(int j = 0; j <= 30; j++){
-            if(pow(2,j) == n){
-                return true;
-            }
-        }
-        return false;
-    }
+    return false;
 }
 
 int main(){
     
-    // cout << isPowerOfTwo(-8);
     cout << (pow(-2,4) == (-8));
 
     return 0;
